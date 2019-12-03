cb02c214d5952251143737943fae766cce5db518
车二亮 <cel105302@163.com>
2019-12-03 15:26:13 +0800

    add sleuth-zipkin demo

--- cactus-springboot/src/main/java/com/cactus/demo/cactusspringboot/unsubmit/controller/SubmitController.java
+++ cactus-springboot/src/main/java/com/cactus/demo/cactusspringboot/unsubmit/controller/SubmitController.java
@@ -1,18 +1,30 @@
 package com.cactus.demo.cactusspringboot.unsubmit.controller;
 
 import com.cactus.demo.cactusspringboot.unsubmit.UnsubmitEnumeration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/")
 public class SubmitController {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("/submit")
-    @UnsubmitEnumeration(isCheck = true)
+    @UnsubmitEnumeration(isCheck = false)
     public String submit(){
+
+        return restTemplate.getForObject("http://localhost:8077/sendTrace", String.class);
+    }
+
+    @GetMapping("/sendTrace")
+    public String sendTrace(){
 
         return "123";
     }
+
 }
